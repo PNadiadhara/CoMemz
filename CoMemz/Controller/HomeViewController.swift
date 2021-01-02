ba2bcfb0f47bda1121d1d8f19957c6958c66a2f7
PN <priteshrna@gmail.com>
2021-01-02 15:28:06 -0500

    file structure update

--- CoMemz/Controller/HomeViewController.swift
+++ CoMemz/Controller/HomeViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handleNotAuthenticated()
+    }
+    // check auth status
+    private func handleNotAuthenticated(){
+        if Auth.auth().currentUser == nil {
+            // display log in
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: true)
+            
+        }
     }
 
 
--- CoMemz/Controller/Login/RegistrationViewController.swift
+++ CoMemz/Controller/Login/RegistrationViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
     
 
  
