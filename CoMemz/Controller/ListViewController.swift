6bac32796567b63f32fb84aa82f3b0ed8e556f90
PN <priteshrna@gmail.com>
2021-01-18 13:46:36 -0500

    follow update

--- CoMemz/Controller/ListViewController.swift
+++ CoMemz/Controller/ListViewController.swift
@@ -11,6 +11,13 @@ class ListViewController: UIViewController {
 
     private let data: [String]
     
+    private let tableview: UITableView = {
+        let tv = UITableView()
+        tv.register(UserFollowTableViewCell.self,
+                    forCellReuseIdentifier: UserFollowTableViewCell.identifier)
+        return tv
+    }()
+    
     init(data: [String]) {
         self.data = data
         super.init(nibName: nil, bundle: nil)
@@ -23,10 +30,37 @@ class ListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        tableview.delegate = self
+        tableview.dataSource = self
+        view.addSubview(tableview)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableview.frame = view.bounds
+    }
+    
+}
 
-   
-
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: UserFollowTableViewCell.identifier, for: indexPath) as! UserFollowTableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        cell.configure(with: "")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableview.deselectRow(at: indexPath, animated: true)
+        // go to profile of cell
+        let model = data[indexPath.row]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
--- CoMemz/Controller/ProfileViewController.swift
+++ CoMemz/Controller/ProfileViewController.swift
@@ -176,3 +176,4 @@ extension ProfileViewController : ProfileTabsCollectionReusableViewDelegate {
     
     
 }
+
